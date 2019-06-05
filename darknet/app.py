aabb353e7915e80d5bf0bb9ccd1fc0c86d21a320
htn274 <thiennust274@gmail.com>
2019-06-05 17:29:44 +0700

    make code more explicit

--- darknet/app.py
+++ darknet/app.py
@@ -14,103 +14,22 @@ def read_obj_names(file):
             Classes.append(line.strip())
 
 class DetectDraw:
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         dn.set_gpu(0)
         self.net = dn.load_net("quick-draw-model/my-yolov3.cfg".encode('utf8'), "quick-draw-model/my-yolov3.weights".encode('utf8'), 0)
         self.meta = dn.load_meta("quick-draw-model/obj.data".encode('utf8'))
     
     def predict(self):
-        r = (dn.detect(self.net, self.meta, "drawing.jpg".encode('utf8')))[0]
+        r = (dn.detect(self.net, self.meta, "drawing.jpg".encode('utf8')))
+        print(r)
+        if len(r) == 0:
+            return None
+        r = r[0]
         # predicted class and accuracy
         return r[0].decode('utf8'), r[1]
 
-class Game:
-    def create_canvas(self):
-        self.cv = Canvas(self.root, width=640, height=480, bg='white')
-        self.cv.bind('<1>', self.activate_paint)
-        self.cv.pack(expand=YES, fill=BOTH)
-
-    def create_draw(self):
-        self.image1 = PIL.Image.new('RGB', (640, 480), 'white')
-        self.draw = ImageDraw.Draw(self.image1)
-
-    def create_button(self, frame, name_btn, cmd_btn, side_btn = LEFT):
-        btn = Button(frame, 
-        text=name_btn, 
-        font = ("Arial", 16),
-        command=cmd_btn,
-        width = 8,
-        bg = "#24252a",
-        fg = "#f03434")
-
-        btn.pack(side = side_btn, padx = 5, pady = 5)
-
-    def draw_live(self, frame):
-        image = Image.open('Image/live.png')
-        img = ImageTk.PhotoImage(image.resize((48, 48))) 
-        label = Label(frame, image=img)
-        label.pack(side=LEFT)
-
-
-    def create_frame_button(self):
-        self.frame_btn = Frame(self.root)
-        self.frame_btn.pack()
-
-        self.draw_live(self.frame_btn)
-        self.btn_submit = self.create_button(self.frame_btn, "Submit", self.submit)
-        self.btn_clear = self.create_button(self.frame_btn, "Clear", self.clear)
-
-    def create_window(self):
-        self.root = Tk()
-        self.root.title("Quick-draw")
-
-    def create_obj_name_label(self):
-        self.obj_name = Label(
-            self.root, 
-            text = "Classes",
-            font = ("Arial", 18),
-            fg = "#013243"
-        )
-        self.obj_name.pack()
-
-    def random_object(self):
-        num_rnd = random.randrange(0, len(Classes), 1)
-        self.obj_name.config(text = "Let draw a/an {}!".format(Classes[num_rnd]))
-
-    def drawing_window(self):
-        self.lastx, self.lasty = None, None
-        self.create_canvas()
-        self.create_draw()
-
-    def __init__(self):
-        # Create window
-        self.create_window()
-        self.create_obj_name_label()
-        self.random_object()
-        self.drawing_window()
-        self.create_frame_button()        
-        
-        # self.detector = DetectDraw()
-        self.root.mainloop()
-
-    def submit(self):
-        filename = f'drawing.jpg'
-        self.image1.save(filename)
-        print("CHECKING.....")
-        ans, accuracy = self.detector.predict()
-        self.check_ans(ans)
-
-    def check_ans(self, ans):
-        if ans == self.obj_name.cget("text"):
-            messagebox.showinfo("Sucess", "Congrats! You are an artist!")
-            self.random_object()
-            self.clear()
-        else:
-            if len(ans) > 0:
-                messagebox.showinfo("Failed", "Wrong! You draw a {}. Try again.".format(ans))
-            else:
-                messagebox.showinfo("Failed!", "I don't know what you drew. Try again")
-
+# Main area contains drawing area and question area
+class MainArea:
     def activate_paint(self, e):
         self.cv.bind('<B1-Motion>', self.paint)
         self.lastx, self.lasty = e.x, e.y
@@ -122,19 +41,112 @@ class Game:
         self.draw.line((self.lastx, self.lasty, x, y), fill='black', width=1)
         self.lastx, self.lasty = x, y
 
-    def clear(self):
-        self.cv.delete('all')
+    def create_canvas(self, _frame):
+        self.cv = Canvas(_frame, width=640, height=480, bg='white')
+        self.cv.bind('<1>', self.activate_paint)
+        self.cv.pack(expand=YES, fill=BOTH)
+
+    def create_draw(self):
+        self.image1 = PIL.Image.new('RGB', (640, 480), 'white')
+        self.draw = ImageDraw.Draw(self.image1)
+    
+    def drawing_window(self, _frame):
+        self.lastx, self.lasty = None, None
+        self.create_canvas(_frame)
         self.create_draw()
-        return
+
+    def create_frame(self):
+        self.top_frame = Frame(self.root)
+        self.top_frame.pack()
+        self.drawing_frame = Frame(self.root)
+        self.drawing_frame.pack()
+
+    def create_obj_name_label(self, _frame):
+        self.obj_name = Label(
+            _frame, 
+            text = "Classes",
+            font = ("Arial", 18),
+            fg = "#013243"
+        )
+        self.obj_name.pack()
+
+    def __init__(self, _root):
+        self.root = Frame(_root, width=640, height=480, relief='sunken', borderwidth=2)
+        self.root.pack(expand=True, fill='both', side='left', anchor='nw')
+        self.create_frame()
+        self.drawing_window(self.drawing_frame)
+        self.create_obj_name_label(self.top_frame)
+
+    def get_canvas(self):
+        return self.cv
+
+    def get_drawing(self):
+        return self.draw
+    
+    def get_image(self):
+        return self.image1
+
+    def set_text_label_obj_name(self, _text):
+        self.obj_name.config(text=_text)
+
+class Game:
+    def create_button(self, frame, name_btn, cmd_btn):
+        btn = Button(frame, 
+        text=name_btn, 
+        font = ("Arial", 16),
+        command=cmd_btn,
+        width = 8,
+        bg = "#24252a",
+        fg = "#f03434")
+        btn.pack(padx = 5, pady = 5)
+
+    def draw_live(self, frame):
+        image = Image.open('Image/live.png')
+        img = ImageTk.PhotoImage(image.resize((48, 48))) 
+        label = Label(frame, image=img)
+        label.pack(side=LEFT)
+
+    def random_obj_name(self):
+        num = random.randrange(0, len(Classes), 1)
+        self.obj_name = Classes[num]
+        self.mainarea.set_text_label_obj_name(self.obj_name)
+
+    def __init__(self):
+        self.root = Tk()
+        self.mainarea = MainArea(self.root)
+        self.sidebar = Frame(self.root, width=200, height=480, relief='sunken', borderwidth=2)
+        self.sidebar.pack(expand=True, fill='both', side='right', anchor='nw')
+        self.btn_submit = self.create_button(self.sidebar, "Submit", self.submit)
+        self.btn_clear = self.create_button(self.sidebar, "Clear", self.clear)
+        self.random_obj_name()
+        self.detector = DetectDraw()
+        self.root.mainloop()
+
+    def submit(self):
+        filename = f'drawing.jpg'
+        self.mainarea.get_image().save(filename)
+        print("CHECKING.....")
+        result = self.detector.predict()
+        self.check_ans(result)
+
+    def check_ans(self, res):
+        if res == None:
+            messagebox.showinfo("Failed!", "I don't know what you drew. Try again")
+            return 
+
+        ans = res[0]
+        if ans == self.obj_name:
+            messagebox.showinfo("Sucess", "Congrats! You are an artist!")
+            self.random_obj_name()
+            self.clear()
+        else:
+            messagebox.showinfo("Failed", "Wrong! You draw a {}. Try again.".format(ans))
+                
+
+    def clear(self):
+        self.mainarea.get_canvas().delete('all')
+        self.mainarea.create_draw()
 
 if __name__ == "__main__":
     read_obj_names('quick-draw-model/obj.names')
     game = Game()
-    # root = Tk()
-    # image = Image.open('Image/live.png')
-    # img = ImageTk.PhotoImage(image.resize((48, 48))) 
-    # label = Label(root, image=img)
-    # label.image = img
-    # label.pack()
-    # root.mainloop()
-
